919574231d8817188b27338be01e262d2b36644b
Hieu Nguyen Minh <hieunguyenm.dev@gmail.com>
2024-04-04 16:07:12 +0700

    Read the .env file and parse the environment variables

--- scripts/db.pm
+++ scripts/db.pm
@@ -1,14 +1,27 @@
 use DBI;
 use DBD::mysql;
 
-use constant K_DB         => 'local';
-use constant K_USER       => 'root';
-use constant K_PWD        => 'ANSKk08aPEDbFjDO';
-use constant K_HOST       => 'mysql.orb.local';
+# Specify the path to your .env file
+my $env_file = './.env';
+
+# Read the .env file and parse the environment variables
+open(my $fh, '<', $env_file) or die "Could not open file '$env_file' $!";
+while (my $line = <$fh>) {
+    chomp $line;
+    my ($key, $value) = split('=', $line, 2);
+    $ENV{$key} = $value;
+}
+close($fh);
+
+# Access the environment variables
+my $K_DB = $ENV{'K_DB'};
+my $K_USER = $ENV{'K_USER'};
+my $K_PWD = $ENV{'K_PWD'};
+my $K_HOST = $ENV{'K_HOST'};
 
 sub connect_to_cnxcc_db()
 {
-	return connect_to_db(K_DB, K_HOST, K_USER, K_PWD);
+	return connect_to_db($K_DB, $K_HOST, $K_USER, $K_PWD);
 }
 
 sub connect_to_db($$$$)
