75df69d661bf253d8e7ddf16a6b1f3c873b7658a
Hieu Nguyen Minh <hieunguyenm.dev@gmail.com>
2024-04-05 09:32:57 +0700

    add log

--- scripts/db.pm
+++ scripts/db.pm
@@ -1,9 +1,20 @@
 use DBI;
 use DBD::mysql;
-use lib '/usr/local/lib64/kamailio/perl/';
-use Kamailio qw ( log );
-use Kamailio::Constants;
-use Kamailio::Message;
+
+use Log::Log4perl;
+use constant L_INFO => 'INFO';
+use constant L_ERR => 'ERROR';
+
+Log::Log4perl::init_and_watch("log4perl.conf", 60);
+my $logger = Log::Log4perl->get_logger();
+
+$logger->info("This is an informational message");
+$logger->error("This is an error message");
+
+# sub log($$) {
+#     my ($level, $message) = @_;
+#     Log::Log4perl::get_logger('kamailio')->log($level, $message);
+# }
 
 # Specify the path to your .env file
 my $env_file = './.env';
@@ -23,24 +34,28 @@ my $K_USER = $ENV{'K_USER'};
 my $K_PWD = $ENV{'K_PWD'};
 my $K_HOST = $ENV{'K_HOST'};
 
-log(L_INFO, "K_DB: $K_DB\n");
-log(L_INFO, "K_USER: $K_USER\n");
-log(L_INFO, "K_PWD: $K_PWD\n");
-log(L_INFO, "K_HOST: $K_HOST\n");
+$logger->info("K_DB: $K_DB");
+$logger->info("K_USER: $K_USER");
+$logger->info("K_PWD: $K_PWD");
+$logger->info("K_HOST: $K_HOST");
+
+# use constant K_DB         => 'kamailio';
+# use constant K_USER       => 'kamailio';
+# use constant K_PWD        => 'kamailiorw';
+# use constant K_HOST       => 'localhost';
 
 sub connect_to_cnxcc_db()
 {
-    log(L_INFO, "Connect to cnxcc database.\n");
+    $logger->info("Connect to cnxcc database.\n");
 	return connect_to_db($K_DB, $K_HOST, $K_USER, $K_PWD);
 }
 
 sub connect_to_db($$$$)
 {
-    log(L_INFO, "Connect to database.\n");
 	my ($db, $host, $user, $pwd) 	= @_;	
     my $dsn = "DBI:mysql:$db:$host";
 
-    log(L_INFO, "DSN: $dsn\n");
+    $logger->info("Dsn: $dsn\n");
 
     return DBI->connect($dsn, $user, $pwd) || die "Could not connect to database: $DBI::errstr";
 }
@@ -49,7 +64,7 @@ sub update_customer_credit($$$)
 {
 	my ($dbh, $customer, $duration)	= @_;
 
-    log(L_INFO, "Charge for customer [$customer] with duration [$duration]\n");
+    $logger->info("Charge for customer [$customer] with duration [$duration]\n");
 	
 	$dbh->do("UPDATE credit_card SET credit_money = credit_money - ($duration * cps) WHERE customer_id = '$customer'") or die("$!\n");
 }
